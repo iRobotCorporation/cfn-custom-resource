8e044b064252a57643441d03944698197eac1413
Unknown <bkehoe@irobot.com>
2018-02-01 13:45:50 -0500

    Removed decryption from SSMParameterInput

--- examples/SSMParameterInput.py
+++ examples/SSMParameterInput.py
@@ -3,7 +3,13 @@ MySSMParameter:
   Type: Custom::SSMParameterInput
   Properties:
     Name: My/Parameter/Name
-    [WithDecryption: True/False]
+
+A drop-in replacement for CloudFormation SSM parameter types
+(see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/parameters-section-structure.html#aws-ssm-parameter-types )
+CloudFormation SSM parameter types don't allow for building the key from other 
+inputs. They also don't allow for SecureString types. This resource will allow
+getting the encrypted value of a SecureString, and the KeyId is available as
+an attribute.
 
 Access value by Ref-ing the resource.
 All outputs from GetParameter are available as attributes.
@@ -17,7 +23,12 @@ class SSMParameterInput(CloudFormationCustomResource):
     def _get(self):
         client = self.get_boto3_client('ssm')
         
-        response = client.get_parameter(**self.resource_properties)
+        kwargs = {
+            'Name': self.resource_properties['Name'],
+            'WithDecryption': False,
+        }
+        
+        response = client.get_parameter(**kwargs)
         
         value = response['Parameter']['Value']
         
