f8d301e1e43fec2d7121967e55c270b64f074796
Unknown <bkehoe@irobot.com>
2018-02-24 08:19:42 -0500

    License updates

--- cfn_custom_resource/utils.py
+++ cfn_custom_resource/utils.py
@@ -1,6 +1,8 @@
-"""
-This module provides a utility function, generate_request, to create events
-for using in testing.
+"""Utilities to use for testing CloudFormationCustomResource.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
 import time
